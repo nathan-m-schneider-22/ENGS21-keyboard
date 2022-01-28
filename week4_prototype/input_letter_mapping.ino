0f6df4e8a0f89b9c442b98a83f432313572488ab
nathan-m-schneider-22 <nathan.m.schneider.22@dartmouth.edu>
2022-01-27 21:19:42 -0500

    arduino code comments

--- week4_prototype/input_letter_mapping.ino
+++ week4_prototype/input_letter_mapping.ino
@@ -2,15 +2,14 @@ char *inputs[] = { "U----","D----","L----","R----","-U---","-D---","-L---","-R--
 char *letters[] = { "E","A","R","I","O","T","N","S","L","C","U","D","P","M","H","G","B","F","Y","W","K","V","X","Z","J","Q" };
 const int NUM_LETTERS =  26 ;
 
-char *map_letter(char* input)
-{
-    for (int i=0;i<NUM_LETTERS;i++){
-      //Serial.println(input);
-      //Serial.println(inputs[i]);
 
-      if(strcmp(input, inputs[i]) == 0) {
-        return letters[i];
+// above are outputs of the generate_layout.py script to generate key layouts
+char *map_letter(char* input)
+{// (wish I had a dict now)
+    for (int i=0;i<NUM_LETTERS;i++){ // iterate over all options 
+      if(strcmp(input, inputs[i]) == 0) { // see if there's a match
+        return letters[i]; // return the match
       }
     }
-    return "?";
+    return "?"; // no match
 }
--- week4_prototype/week4_prototype.ino
+++ week4_prototype/week4_prototype.ino
@@ -1,6 +1,7 @@
 // Nathan Schneider
 // Week 4 Prototype
 
+// Constants for inputs
 const int NUM_KEYS = 5;
 const int D = 512;
 const int MINVAL = 250;
@@ -9,44 +10,46 @@ const int THRESHOLD = 80;
 const int COOLDOWN = 200;
 const int TEST_KEY = 0;
 
+
+// Initialize joystick readings to the middle range
 int xvals[NUM_KEYS] = {D, D, D, D, D};
 int yvals[NUM_KEYS] = {D, D, D, D, D};
 
+// Stick directions, expressed by 5 characters (U,D,L,R)
 char directions[NUM_KEYS] = "-----";
 
-#include <Keyboard.h>
+// keyboard not working on Arduino Uno
+//#include <Keyboard.h> 
 
 void setup()
 {
   Serial.begin(9600);
+
+// keyboard not working on Arduino Uno
 //  Keyboard.begin();
 
 }
 
 void loop()
 {
-  read_keys();
-  get_directions();
-  //Serial.println(directions);
-  char* letter = map_letter(directions);
+  read_keys(); // read the joystick inputs
+  get_directions(); //convert input voltages into directions
+
+  char* letter = map_letter(directions); // map directions to a letter through the generated layout
   if (strcmp(letter, "?") != 0){
     Serial.println(letter);
-    delay(COOLDOWN);
+    delay(COOLDOWN); // if a real letter is typed, cool down before the next reading
   }
 
 }
 void get_directions()
 {
   for (int i = 0; i < NUM_KEYS; i++)
-  {
+  { // Map the inputs from -100 to 100
     int mapped_x = map(xvals[i], MINVAL, MAXVAL, 100, -100);
     int mapped_y = map(yvals[i], MINVAL, MAXVAL, -100, 100);
-    //    Serial.println(xvals[i]);
-    //    Serial.println(yvals[i]);
-    //    Serial.println(mapped_x);
-    //    Serial.println(mapped_y);
-    //    Serial.println(" ");
-    if (mapped_x < -THRESHOLD)
+    
+    if (mapped_x < -THRESHOLD) // if surpasses a threshold, assign a direction
       directions[i] = 'L';
     else if (mapped_x > THRESHOLD)
       directions[i] = 'R';
@@ -59,7 +62,7 @@ void get_directions()
   }
 }
 
-void read_keys()
+void read_keys() // reads joysticks, currently only reads TEST_KEY
 {
   int sensorValue = analogRead(A0);
   int sensorValue2 = analogRead(A1);
