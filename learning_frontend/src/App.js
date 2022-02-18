598bfe1471f070efcfcb799eb338e98a7ae2cd52
JRickenbacher <jason.rickenbacher@yahoo.com>
2022-02-18 01:51:27 -0500

    Adding shift functionality to UI. Not yet working

--- learning_frontend/src/App.js
+++ learning_frontend/src/App.js
@@ -1,17 +1,42 @@
+import React from "react";
 import GuideBlock from './components/GuideBlock';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 style={{ textAlign: "center" }}>Guide to Your Custom Layout</h1>
-      <GuideBlock />
-      <div style={{ textAlign: "center", marginTop: "2em" }}>
-        <textarea style={{ fontSize: '36px' }} name="paragraph_text" cols="50" rows="10"></textarea>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      shiftActive: false,
+      tempKey: null,
+    };
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1 style={{ textAlign: "center" }}>Guide to Your Custom Layout</h1>
+        <GuideBlock />
+        <div style={{ textAlign: "center", marginTop: "2em" }}>
+          <textarea style={{ fontSize: '36px' }} name="paragraph_text" cols="50" rows="10" onKeyDown={(e) => this.handleKeyDown(e)} onKeyUp={(e) => this.handleKeyUp(e)}></textarea>
+          <p>{this.state.tempKey || "none"}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  handleKeyDown(e) {
+    if(!this.state.shiftActive && (this.e.key === "Shift" || this.e.keyCode === 16)) {
+      this.setState({ shiftActive: true });
+    }
+    this.setState({ tempKey: e.key });
+    console.log(e);
+  };
+
+  handleKeyUp(e) {
+    if(this.state.shiftActive && (this.e.key === "Shift" || this.e.keyCode === 16)) {
+      this.setState({ shiftActive: false });
+    }
+  };
+
 }
 
-//const handleKeyDown = ()
 
-export default App;
+//const handleKeyDown = ()
